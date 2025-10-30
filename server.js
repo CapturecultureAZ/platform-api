845a2ba40cf9057a3ec571c6ef6bbe4434e5fb8f
Ricardo Cordova <YOUR_GITHUB_EMAIL@example.com>
2025-10-30 15:43:40 -0700

    Add /api/ping

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.get('/api/ping', (_req, res) => res.json({ ok: true, pong: true }));
 // Blank homepage so root URL shows something
 app.get('/', (_req, res) => {
   res.send(`
